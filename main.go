425acd387d389d5b1ab8ab0f987312a9732685fa
alex <2844520415@qq.com>
2022-10-03 13:04:31 +0800

    feat: add global main entry

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	hi := "hello"
+	fmt.Printf("hello world!!\n %s: type of  %T", hi, hi)
+	// a := 1
+	// b := 1
+	// fmt.Print(a)
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Print(" ", b)
+	// 	a, b = b, a+b
+	// }
+}
