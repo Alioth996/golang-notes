99f7af664e412956a0a849a874fb2f4cfc36f06b
alex <2844520415@qq.com>
2022-10-03 21:55:02 +0800

    feat: add array slice method

--- src/ch6/array_ch/array_test.go
+++ src/ch6/array_ch/array_test.go
@@ -3,5 +3,24 @@ package arraych_test
 import "testing"
 
 func TestArrayBegin(t *testing.T) {
+	// var arr [3]int 声明长度为3,元素类型为int的数组
 
+	// arr := [3]int{}  数组声明简写
+
+	arr := [...]int{1, 2, 30} // 声明数组并初始化数组元素
+
+	// 遍历数组 range
+	for _, v := range arr {
+		t.Log(v)
+	}
+	t.Log(len(arr))
+
+	// 多维数组
+	// var arr1 [3][4]int
+
+	arr1 := [...][2]int{{1, 2}, {2, 3}, {2}}
+
+	t.Log(arr1)
+	t.Logf("arr1 len %d", len(arr1))
 }
+
