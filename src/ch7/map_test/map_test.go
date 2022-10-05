c07a89e01cc5a8e6d7c99a36fbda3e3138ae2aa4
alex <2844520415@qq.com>
2022-10-05 23:56:22 +0800

    feat: finsh  all  Map key test

--- src/ch7/map_test/map_test.go
+++ src/ch7/map_test/map_test.go
@@ -12,3 +12,20 @@ func TestMapDefine(t *testing.T) {
 	t.Log(m2, m3)
 	// t.Log(cap(m2))  Map 不能使用cap()函数求容量
 }
+
+func TestAccessNotExistKey(t *testing.T) {
+	m := map[string]int{"one": 1, "two": 2}
+
+	// map中的key不存在返回0值,不能通过返回nil判断元素是否存在
+	t.Log(m["three"]) // m[three] = 0
+
+	// 判断key-value是否存在
+	if v, ok := m["three"]; ok {
+		t.Logf("m[three] = %d", v)
+	} else {
+		// 手动添加 m["three"] = 0
+		m["three"] = 0
+		t.Log(m)
+	}
+
+}
