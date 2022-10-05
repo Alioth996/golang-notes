68d86e612221a3bfc465872739c7804f0ae25fd0
alex <2844520415@qq.com>
2022-10-05 23:44:00 +0800

    feat: add map define test

---
+++ src/ch7/map_test/map_test.go
@@ -0,0 +1,13 @@
+package map_test
+
+import "testing"
+
+func TestMapDefine(t *testing.T) {
+	// 3.声明并初始化
+	m2 := map[string]int{"one": 1, "two": 2}
+
+	// 4.make关键字 适合已知容量
+	m3 := make(map[string]int, 10 /*cap*/)
+
+	t.Log(m2, m3)
+}
