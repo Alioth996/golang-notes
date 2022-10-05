bd2266b49a98a0debf6ea1163ca5b987009fdd9e
alex <2844520415@qq.com>
2022-10-05 23:22:48 +0800

    feat: test slice share memory

--- src/ch6/slice_ch/slice_test.go
+++ src/ch6/slice_ch/slice_test.go
@@ -1,6 +1,8 @@
 package slicech_test
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSliceBeginByVar(t *testing.T) {
 	// slice简单理解为可变长度的数组
@@ -32,7 +34,29 @@ func TestSlicebyMake(t *testing.T) {
 	//创建一个可访问长度为5,容量为10,填充元素为0 的切片,后续长度超出,则容量翻倍.并且容量总是翻倍向后拓展
 	pointList := make([]float64, 5, 5)
 	t.Log(priceList)
-	pointList = append(pointList, 1.5)
-	t.Log(pointList, cap(pointList)) // 此时cap=20
+	pointList = append(pointList, 1.5) // append返回一个新切片,把之前的切片拷贝一份,并开辟一份cap大小的存储空间,可能会引起性能问题
+	t.Log(pointList, cap(pointList))   // 此时cap=20
 
+}
+
+func TestSliceShareMem(t *testing.T) {
+	// 切片共享数据
+	months := []string{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+
+	// 研究切片的len与cap变化
+	spring := months[:3]
+	summer := months[3:6]
+	autumn := months[6:9]
+	winner := months[9:]
+
+	springLen := len(spring) // 3
+	springCap := cap(spring) // 12
+
+	summerLen := len(summer) // 3
+	summerCap := cap(summer) // 9
+
+	t.Log(springLen, springCap)
+	t.Log(summerLen, summerCap)
+
+	t.Log(spring, summer, autumn, winner)
 }
